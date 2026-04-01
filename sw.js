be0f92d2df601fcfa9f435df713fddff26564125
thg139426-web <thg139426@gmail.com>
2026-04-01 13:51:52 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "thgjb-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icons/icon-192.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
